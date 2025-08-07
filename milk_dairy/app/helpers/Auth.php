2e075440bef3b4f3c28bdddb9cff9ae0b3c0420b
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-07 16:30:54 +0530

    Update Auth.php

--- milk_dairy/app/helpers/Auth.php
+++ milk_dairy/app/helpers/Auth.php
@@ -7,7 +7,7 @@ class Auth
         session_start();
          Auth::isLoggedIn(); // Check if user is already logged in
         if (!isset($_SESSION['vendor'])) {
-            header("Location: /public/auth/logout");
+            header("Location: /public/auth/login");
             exit;
         }
     }
@@ -16,7 +16,7 @@ class Auth
         if(isset($_SESSION['vendor']) || isset($_COOKIE['vendor'])){
             $_SESSION['vendor'] = $_SESSION['vendor'] ?? $_COOKIE['vendor'];
             // Optional: Regenerate session ID for security
-            header("Location: /public/auth/logout");
+            header("Location: /public/dashboard");
             exit;
         }
     }
