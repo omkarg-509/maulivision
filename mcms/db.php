a456eb96dda13aace984a2c29b07ae5a11ffb0f5
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-13 13:14:07 +0530

    Update db.php

--- mcms/db.php
+++ mcms/db.php
@@ -1,11 +1,1 @@
-<?php
-try {
-    $conn = new PDO(
-        'mysql:host=localhost;dbname=u367009900_maulivision;charset=utf8mb4',
-        'u367009900_maulivision',
-        'S54/t&XCIup+'
-    );
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
+<?php $conn = new mysqli('localhost', 'u367009900_maulivision', 'S54/t&XCIup+', 'u367009900_maulivision');?>
