056e8526ba294e0c6d63e12302b274e5b2f5d433
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-14 11:39:33 +0530

    Auto commit on Thu, Aug 14, 2025 11:39:33 AM

--- milk_dairy/public/index.php
+++ milk_dairy/public/index.php
@@ -2,14 +2,9 @@
 // ini_set('display_errors', 1);
 // error_reporting(E_ALL);
 
-require_once '../config/config.php';
-require_once '../core/App.php';
-require_once '../core/Controller.php';
-
+require_once '../config/config.php';  // Load configuration
+require_once '../core/App.php';   // Load application core
+require_once '../core/Controller.php';  // Load base controller
 
 $app = new App(); 
-
-
-
-?>
 
