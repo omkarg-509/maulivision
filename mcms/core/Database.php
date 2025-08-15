27b6f5794e0b67dbd09d955ca78dba2f27033073
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-15 08:47:20 +0530

    Auto commit on Fri, Aug 15, 2025  8:47:20 AM

--- mcms/core/Database.php
+++ mcms/core/Database.php
@@ -6,7 +6,7 @@ class Database
 
     public function __construct()
     {
-        $this->db = new mysqli("localhost", "u367009900_mcms", "oWM9Xascu?8", "u367009900_mcms");
+        $this->db = new mysqli("localhost", "u367009900_mcms", "5P[lKbV^^v&", "u367009900_mcms");
         if ($this->db->connect_error) {
             die("Database connection failed: " . $this->db->connect_error);
         }
