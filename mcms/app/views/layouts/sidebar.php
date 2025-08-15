7ff556a39db4d21b011862c19ba6337bf6f4ab27
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-15 12:04:57 +0530

    Auto commit on Fri, Aug 15, 2025 12:04:57 PM

--- mcms/app/views/layouts/sidebar.php
+++ mcms/app/views/layouts/sidebar.php
@@ -6,15 +6,8 @@
           <li class="dropdown"><a href="#" data-bs-toggle="dropdown"
               class="nav-link dropdown-toggle nav-link-lg nav-link-user">
               <img alt="image" src="<?=BASE_URL?>/assets/img/user.png" class="user-img-radious-style">
-              <span class="d-sm-none d-lg-inline-block"></span></a>
-            <div class="dropdown-menu dropdown-menu-left">
-              <div class="dropdown-title">Hello <?=$_SESSION['vendor']['email']?></div>
-              
-              <div class="dropdown-divider"></div>
-              <a href="<?=BASE_URL?>auth/logout" class="dropdown-item has-icon text-danger">
-                <i class="fas fa-sign-out-alt"></i> Logout
-              </a>
-            </div>
+
+            </a>
           </li>
         </ul>
           
