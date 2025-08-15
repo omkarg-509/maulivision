c8b6fb3737b98e1e94d536c0ffcbc0acdc3f2cfe
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-15 21:32:56 +0530

    Auto commit on Fri, Aug 15, 2025  9:32:56 PM

--- mcms/app/views/customer/index.php
+++ mcms/app/views/customer/index.php
@@ -45,48 +45,48 @@
                       <tbody>
                         <?php
 
-                        $today = date('Y-m-d');
-                        $hasTodayEntries = false;
+                        $selectedDate = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
+                        $hasEntries = false;
 
                         if (!empty($data['customers'])):
                           foreach ($data['customers'] as $index => $cust):
-                            // Check if created_at is today
+                            // Check if created_at matches selected date
                             $entryDate = isset($cust['created_at']) ? date('Y-m-d', strtotime($cust['created_at'])) : '';
-                            if ($entryDate === $today):
-                              $hasTodayEntries = true;
+                            if ($entryDate === $selectedDate):
+                              $hasEntries = true;
                         ?>
-                          <tr>
-                            <td><?= $index + 1 ?></td>
-                            <td><?= htmlspecialchars($cust['name']) ?></td>
-                            <td><?= htmlspecialchars($cust['mobile']) ?></td>
-                            <td><?= htmlspecialchars(ucfirst($cust['in_time'])) ?></td>
-                            <td><?= htmlspecialchars($cust['amount']) ?></td>
-                            <td><?= htmlspecialchars($cust['staff']) ?></td>
-                            <td><?= htmlspecialchars($cust['payment_method']) ?></td>
-                            <td>
-                              <a href="/public/dailyentry/delete/<?= urlencode($cust['id']) ?>"
-                                 onclick="return confirm('Are you sure you want to delete this milk Entries?');"
-                                 title="Delete">
-                                <i class="fa fa-trash text-danger"></i>
-                              </a>
-                            </td>
-                          </tr>
+                              <tr>
+                                <td><?= $index + 1 ?></td>
+                                <td><?= htmlspecialchars($cust['name']) ?></td>
+                                <td><?= htmlspecialchars($cust['mobile']) ?></td>
+                                <td><?= htmlspecialchars(ucfirst($cust['in_time'])) ?></td>
+                                <td><?= htmlspecialchars($cust['amount']) ?></td>
+                                <td><?= htmlspecialchars($cust['staff']) ?></td>
+                                <td><?= htmlspecialchars($cust['payment_method']) ?></td>
+                                <td>
+                                  <a href="/public/dailyentry/delete/<?= urlencode($cust['id']) ?>"
+                                     onclick="return confirm('Are you sure you want to delete this milk Entries?');"
+                                     title="Delete">
+                                    <i class="fa fa-trash text-danger"></i>
+                                  </a>
+                                </td>
+                              </tr>
                         <?php
                             endif;
                           endforeach;
-                          if (!$hasTodayEntries):
+                          if (!$hasEntries):
                         ?>
-                          <tr>
-                            <td colspan="6" class="text-center">No customers found for today.</td>
-                          </tr>
+                            <tr>
+                              <td colspan="8" class="text-center">No customers found for selected date.</td>
+                            </tr>
                         <?php
                           endif;
                         else:
                         ?>
-                          <tr>
-                            <td colspan="6" class="text-center">No customers found.</td>
-                          </tr>
-                        <?php endif; ?>   </tbody>
+                            <tr>
+                              <td colspan="8" class="text-center">No customers found.</td>
+                            </tr>
+                        <?php endif; ?>
                     </table>
                   </div>
                 </div>
