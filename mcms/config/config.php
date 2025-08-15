83b9c31f1918c819a2d948b2aa23368d2b3ac1dc
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-15 06:08:36 +0530

    Auto commit on Fri, Aug 15, 2025  6:08:36 AM

--- mcms/config/config.php
+++ mcms/config/config.php
@@ -37,13 +37,13 @@ class Header {
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   
   <!-- General CSS Files -->
-  <link rel="stylesheet" href="../assets/css/app.min.css">
+  <link rel="stylesheet" href="assets/css/app.min.css">
   <!-- Template CSS -->
-  <link rel="stylesheet" href="../assets/css/style.css">
-  <link rel="stylesheet" href="../assets/css/components.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/components.css">
   <!-- Custom style CSS -->
-  <link rel="stylesheet" href="../assets/css/custom.css">
-  <link rel='shortcut icon' type='image/x-icon' href='../assets/img/favicon.ico' />
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
 </head>
 
 
@@ -127,16 +127,16 @@ class Footer {
     <script src="../js/script.js"></script>
 
    <!-- General JS Scripts -->
-  <script src="../assets/js/app.min.js"></script>
+  <script src="assets/js/app.min.js"></script>
   <!-- JS Libraies -->
-  <script src="../assets/bundles/chartjs/chart.min.js"></script>
-  <script src="../assets/bundles/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/bundles/chartjs/chart.min.js"></script>
+  <script src="assets/bundles/apexcharts/apexcharts.min.js"></script>
   <!-- Page Specific JS File -->
-  <script src="../assets/js/page/index.js"></script>
+  <script src="assets/js/page/index.js"></script>
   <!-- Template JS File -->
-  <script src="../assets/js/scripts.js"></script>
+  <script src="assets/js/scripts.js"></script>
   <!-- Custom JS File -->
-  <script src="../assets/js/custom.js"></script>
+  <script src="assets/js/custom.js"></script>
 </body>
 </html>
 
