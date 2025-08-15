1c1abb81a96939a4fe8d8b359ceeaf94b51ece92
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-15 06:01:49 +0530

    Auto commit on Fri, Aug 15, 2025  6:01:48 AM

--- mcms/config/config.php
+++ mcms/config/config.php
@@ -29,7 +29,27 @@ $connection = (new Database())->getConnection();
 
 class Header {
     public function render() {
-        echo "<header><h1>Welcome to MCMS</h1></header>";
+      ?>
+      <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="../assets/css/app.min.css">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/components.css">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="../assets/css/custom.css">
+  <link rel='shortcut icon' type='image/x-icon' href='../assets/img/favicon.ico' />
+</head>
+
+
+<body>
+
+      <?php
     }
 }
 
