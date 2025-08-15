16b560615339f9c4d68262e744cb895e638dceac
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-15 12:17:18 +0530

    Auto commit on Fri, Aug 15, 2025 12:17:18 PM

--- mcms/app/views/customer/index.php
+++ mcms/app/views/customer/index.php
@@ -9,16 +9,9 @@
   <section class="section">
      
           </div>
-         <div class="col-lg-12 col-md-12 col-12 col-sm-12">
-            <div class="col-12 text-end"><a class="btn btn-primary mb-3 " href="/public/customer/create">Add Customer</a></div>
-            
-  <div class="card">
-    <div class="card-header">
-      <h4>Customers Details</h4>
-    </div>
- <div class="card-body">
+        
                     
-                      <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+            <div class="col-lg-12 col-md-12 col-12 col-sm-12">
               <div class="card">
                 <div class="card-header">
                   <h4>Customers Details</h4>
@@ -86,10 +79,7 @@
                 </div>
               </div>
             </div>
-      </div>
-    </div>
-  </div>
-
+            
 
         </section>       
     </div>
