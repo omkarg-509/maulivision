3fcb2db3c323b9962db8e7d0eb3a584c0900b59b
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-15 06:26:09 +0530

    Auto commit on Fri, Aug 15, 2025  6:26:09 AM

---
+++ mcms/signup.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+require_once 'config/config.php'; // Ensure this file is included to set up the Database class
+
+?>
+<?=(new Header())->render();?>
+
+<div class="main-wrapper main-wrapper-1">
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        <div class="card card-primary">
+                            <div class="col-12 text-center mt-2">
+                                <h5>Create an Account</h5>
+                            </div>
+                            <div class="card-body">
+
+                                <div id="signup-error" style="color:red; display:none;"></div>
+
+                                <form id="signupForm">
+                               
+                                    <div class="form-group">
+                                        <label for="username">Username</label>
+                                        <input id="username" type="text" class="form-control" name="username" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password" class="control-label">Password</label>
+                                        <input id="password" type="password" class="form-control" name="password" required>
+                                    </div>
+                                    <div class="form-group col-12 text-center">
+                                        <button type="submit" class="btn btn-primary btn-lg btn-block">
+                                            Sign Up
+                                        </button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+    $('#signupForm').on('submit', function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'config/signup.php',
+            type: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function(response) {
+                if (response.status === 'success') {
+                    window.location.href = response.redirect;
+                } else {
+                    $('#signup-error').text(response.message).show();
+                }
+            },
+            error: function() {
+                $('#signup-error').text('Something went wrong.').show();
+            }
+        });
+    });
+    </script>
+</div>
+
+<?=(new Footer())->render();?>
