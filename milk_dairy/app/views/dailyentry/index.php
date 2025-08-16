c2dbc467a04de16cf6431c139e192cff78a474ee
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-17 05:13:42 +0530

    Auto commit on Sun, Aug 17, 2025  5:13:42 AM

--- milk_dairy/app/views/dailyentry/index.php
+++ milk_dairy/app/views/dailyentry/index.php
@@ -159,6 +159,8 @@
 
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
 $(document).ready(function() {
 
