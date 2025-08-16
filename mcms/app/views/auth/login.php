0a5ed8682224b91186bf8993e08c6d43e4b6034d
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-16 11:29:13 +0530

    Auto commit on Sat, Aug 16, 2025 11:29:12 AM

--- mcms/app/views/auth/login.php
+++ mcms/app/views/auth/login.php
@@ -34,8 +34,8 @@
     </div>
   </section>
 </div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+
 <script>
 $('#loginForm').on('submit', function(e) {
     e.preventDefault();
