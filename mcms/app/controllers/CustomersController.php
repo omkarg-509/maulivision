14eb5058bf765defb54569a55605546c577777a2
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-16 14:57:43 +0530

    Update CustomersController.php

--- mcms/app/controllers/CustomersController.php
+++ mcms/app/controllers/CustomersController.php
@@ -9,7 +9,7 @@ class CustomersController extends Controller
         Auth::check();
         $customersModel = $this->model('Customers');
         $customers = $customersModel->getAll();
-        $this->view('/customers/index', ['customers' => $customers]);
+        $this->view('customers/index', ['customers' => $customers]);
     }
 
     public function history()
@@ -17,14 +17,15 @@ class CustomersController extends Controller
         Auth::check();
         $customersModel = $this->model('Customers');
         $customers = $customersModel->getAll();
-        $this->view('/customers/history', ['customers' => $customers]);
+        $this->view('customers/history', ['customers' => $customers]);
     }
 
     public function store()
     {
         $customersModel = $this->model('Customers');
         $customersModel->insert($_POST);
-        header("Location: ". $_SERVER['HTTP_REFERER']."");
+        $customers = $customersModel->getAll();
+        $this->view('customers/index', ['customers' => $customers]);
     }
 
     public function delete($id)
