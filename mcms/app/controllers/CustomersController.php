4eb5bca48d97a6b2b5ffcb483b4b680d9d857839
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-16 13:11:36 +0530

    Auto commit on Sat, Aug 16, 2025  1:11:35 PM

--- mcms/app/controllers/CustomersController.php
+++ mcms/app/controllers/CustomersController.php
@@ -1,25 +1,26 @@
 <?php
 
 require_once '../app/helpers/Auth.php';
+
 class CustomersController extends Controller
 {
-    public function index(){
-     Auth::check();
-     $customersModel = $this->model('Customers');
-     $customersModel->getAll();
-     $this->view('/customers/index',['customers' => $customers]);
-    }
-    public function history(){
+    public function index()
+    {
         Auth::check();
         $customersModel = $this->model('Customers');
         $customers = $customersModel->getAll();
+        $this->view('/customers/index', ['customers' => $customers]);
+    }
 
+    public function history()
+    {
+        Auth::check();
+        $customersModel = $this->model('Customers');
+        $customers = $customersModel->getAll();
         $this->view('/customers/history', ['customers' => $customers]);
     }
 
-    
-
-      public function store()
+    public function store()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
             $customersModel = $this->model('Customers');
@@ -28,12 +29,12 @@ class CustomersController extends Controller
         header("Location: /customers/index");
         exit;
     }
-  public function delete($id)
+
+    public function delete($id)
     {
-        $customerModel = $this->model('Customer');
-        $customerModel->delete($id);
-        header("Location: ". $_SERVER['HTTP_REFERER']."");
+        $customersModel = $this->model('Customers');
+        $customersModel->delete($id);
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+        exit;
     }
-
-
 }
