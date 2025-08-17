59a1e2659346881b5d18939db54177850afa7a1d
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-17 16:50:35 +0530

    Auto commit on Sun, Aug 17, 2025  4:50:35 PM

--- milk_dairy/public/index.php
+++ milk_dairy/public/index.php
@@ -2,10 +2,7 @@
 // ini_set('display_errors', 1);
 // error_reporting(E_ALL);
 // In your router logic
-if ($url[0] == 'subscription') {
-    $controller = new SubscriptionController();
-    $controller->index();
-}
+
 require_once '../config/config.php';  // Load configuration
 require_once '../core/App.php';   // Load application core
 require_once '../core/Controller.php';  // Load base controller
