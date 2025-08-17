dea52ed086b758ca1a2d9c137ed196e1e7e7c9e8
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-17 11:59:40 +0530

    Auto commit on Sun, Aug 17, 2025 11:59:39 AM

--- milk_dairy/app/views/customer/create.php
+++ milk_dairy/app/views/customer/create.php
@@ -138,10 +138,18 @@
                         `<tr>
                            <td>${idx + 1}</td>
                            <td>${cust.bill_id}</td>
-                             <td>${cust.name}</td>
-                             <td>${cust.mobile}</td>
                              <td>
-                             <button class="btn btn-danger btn-sm delete-cust" data-id="${cust.id}">Delete</button>
+                              
+                               ${cust.name}
+                              
+                             </td>
+                             <td> <a href="tel:${cust.mobile}" title="Call ${cust.mobile}">${cust.mobile ? cust.mobile.replace(/^(\d{5})\d+$/, '$1...') : ''} </a></td>
+                             <td>
+                             <button class="btn btn-danger btn-sm delete-cust" data-id="${cust.id}"><i class="fa fa-trash"></i></button>
+                             <a href="/public/customer/show/${cust.id}" 
+                     title="View" class="btn btn-info btn-sm" style="margin-left: 8px;">
+                    <i class="fa fa-eye"></i>
+                    </a>
                              </td>
                          </tr>`
                       );
