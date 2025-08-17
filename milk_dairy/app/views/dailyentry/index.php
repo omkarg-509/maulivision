8bde9f273bba5a8e2e3ee694c46b93a0ecaf35a0
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-17 09:55:20 +0530

    Auto commit on Sun, Aug 17, 2025  9:55:19 AM

--- milk_dairy/app/views/dailyentry/index.php
+++ milk_dairy/app/views/dailyentry/index.php
@@ -23,7 +23,7 @@
                     <input type="hidden" class="form-control" name="vid" value="<?php echo htmlspecialchars($_SESSION['vendor']['id'] ?? ''); ?>" readonly>
                     <div class="form-group row mb-3">
                       <label class="col-sm-3 col-form-label text-center">Date & Time</label>
-                      <div class="col-sm-9 position-relative">
+                      <div class="col-sm-9 position-relative text-center">
                         <input type="text" class="form-control" id="indian_datetime" name="entry_datetime" disabled>
                       </div>
                     </div>
