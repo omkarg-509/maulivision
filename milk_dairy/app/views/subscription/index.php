f80eb6994f926182b59c907d2170bcc9f48ca1ec
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-17 23:35:58 +0530

    Auto commit on Sun, Aug 17, 2025 11:35:58 PM

--- milk_dairy/app/views/subscription/index.php
+++ milk_dairy/app/views/subscription/index.php
@@ -3,23 +3,25 @@
 
 <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
 <script>
-document.getElementById('rzp-button').onclick = function(e){
-    var options = {
-        "key": "9VaUdxAxxPjJHBb0UVURRZ3T", // Enter the Key ID generated from the Dashboard
-        "amount": 1000, // Amount is in currency subunits. 1000 = 10.00
-        "currency": "INR",
-        "name": "Milk Dairy",
-        "description": "Subscription Payment",
-        "handler": function (response){
-            // Redirect after payment success
-            window.location.href = "thank-you.php";
-        },
-        "theme": {
-            "color": "#3399cc"
-        }
-    };
-    var rzp1 = new Razorpay(options);
-    rzp1.open();
-    e.preventDefault();
-}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('rzp-button').onclick = function(e){
+        var options = {
+            "key": "9VaUdxAxxPjJHBb0UVURRZ3T", // Enter the Key ID generated from the Dashboard
+            "amount": 1000, // Amount is in currency subunits. 1000 = 10.00
+            "currency": "INR",
+            "name": "Milk Dairy",
+            "description": "Subscription Payment",
+            "handler": function (response){
+                // Redirect after payment success
+                window.location.href = "thank-you.php";
+            },
+            "theme": {
+                "color": "#3399cc"
+            }
+        };
+        var rzp1 = new Razorpay(options);
+        rzp1.open();
+        e.preventDefault();
+    }
+});
 </script>
