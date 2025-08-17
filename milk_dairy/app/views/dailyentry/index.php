7cbb1593059e34ef6a061a472f6736ba7812e621
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-17 09:53:50 +0530

    Auto commit on Sun, Aug 17, 2025  9:53:50 AM

--- milk_dairy/app/views/dailyentry/index.php
+++ milk_dairy/app/views/dailyentry/index.php
@@ -23,9 +23,11 @@
                     <input type="hidden" class="form-control" name="vid" value="<?php echo htmlspecialchars($_SESSION['vendor']['id'] ?? ''); ?>" readonly>
                     <div class="form-group row mb-3">
                       <label class="col-sm-3 col-form-label text-center">Date & Time</label>
-                      <div class="col-sm-9">
+                      <div class="col-sm-9 position-relative">
                         <input type="text" class="form-control" id="indian_datetime" name="entry_datetime" disabled>
-                      </div><br>
+                      </div>
+                    </div>
+
                       <script>
                         function updateIndianDateTime() {
                           const now = new Date();
@@ -41,6 +43,7 @@
                         updateIndianDateTime();
                         setInterval(updateIndianDateTime, 1000);
                       </script>
+                      <div class="col-sm-9 position-relative">
                       <label class="col-sm-3 col-form-label text-center">Customer Name</label>
                       <div class="col-sm-9 position-relative">
                         <input type="text" class="form-control" id="customer_search" placeholder="Enter customer name or number" required>
