1a70705dda53791953f2581cc0a423f03fc9876e
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-17 16:48:17 +0530

    Auto commit on Sun, Aug 17, 2025  4:48:17 PM

--- milk_dairy/app/views/subscription/index.php
+++ milk_dairy/app/views/subscription/index.php
@@ -1,9 +1,9 @@
 
 <h2>Subscribe Now</h2>
-<form>
-  <script src="https://cdn.razorpay.com/static/widget/subscription-button.js"
-    data-subscription_button_id="pl_R6MzeKIkCKCB3M"
-    data-button_theme="rzp-dark-standard"
-    async>
-  </script>
-</form>
+    <form>
+    <script src="https://checkout.razorpay.com/v1/payment-button.js"
+     data-payment_button_id="pl_R6Njq50b731NJA"
+     async> 
+    </script>
+    </form>
+
