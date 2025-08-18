2509c5175096bfa27920dbbc840b604d30434026
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-19 00:02:51 +0530

    Auto commit on Tue, Aug 19, 2025 12:02:51 AM

--- milk_dairy/app/controllers/CustomerController.php
+++ milk_dairy/app/controllers/CustomerController.php
@@ -96,7 +96,7 @@ class CustomerController extends Controller
 
         if ($customerId && $vid) {
             // Get daily entries for this customer and vendor
-            $milk_entries = $customerModel->DailyEntries($vid, $customerId);
+            $milk_entries = $customerModel->DailyEntries(1, 38);
 
             // Group entries by day (date)
             $grouped = [];
