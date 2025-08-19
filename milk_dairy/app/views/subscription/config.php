db8ea8e4d30d32c171b60eaaf69daf3b6b68f5d2
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-19 14:40:38 +0530

    Auto commit on Tue, Aug 19, 2025  2:40:37 PM

--- milk_dairy/app/views/subscription/config.php
+++ milk_dairy/app/views/subscription/config.php
@@ -4,10 +4,9 @@
 $razorpay_config = array(
     // 'api_key' => 'rzp_test_Y2wy8t1wD1AFaA',
     // 'api_secret' => 'zSqRMpIa2ljBBpkieFYGmfLa',
-    $api_key = 'rzp_test_R6Ull7QpWzBK6X';
-$api_secret = 'oVKvMtV2cAxiNrlYxpe7uf0i';
-    'api_key' => $api_key,
-    'api_secret' => $api_secret
+
+    'api_key' => 'rzp_test_R6Ull7QpWzBK6X',
+    'api_secret' => 'oVKvMtV2cAxiNrlYxpe7uf0i'
 );
 
 // Other configurations, if needed
