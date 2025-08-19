24ab175a3361e4b9dba6f0f581939b030dc0f304
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-19 10:42:46 +0530

    Auto commit on Tue, Aug 19, 2025 10:42:46 AM

--- mcms/app/controllers/StaffController.php
+++ mcms/app/controllers/StaffController.php
@@ -57,7 +57,7 @@ class StaffController extends Controller
         }
     }
 
-      public function listStaff()
+      public function list()
     {
         Auth::check();
         $staffModel = $this->model('Staff');
