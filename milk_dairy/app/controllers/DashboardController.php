467f130eb0818a98ca97caec0bc0428aceb08ea8
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-20 07:30:30 +0530

    Auto commit on Wed, Aug 20, 2025  7:30:29 AM

--- milk_dairy/app/controllers/DashboardController.php
+++ milk_dairy/app/controllers/DashboardController.php
@@ -8,21 +8,6 @@ class DashboardController extends Controller
         Auth::check(); // If using auth
         $this->view('dashboard/index');
     }
-        public function toggleDarkMode()
-    {
-        if (session_status() === PHP_SESSION_NONE) session_start();
-        $user_id = isset($_SESSION['vendor']['id']) ? $_SESSION['vendor']['id'] : null;
-        if (!$user_id) {
-            echo json_encode(['status' => 'error', 'message' => 'Not logged in.']);
-            exit;
-        }
-        require_once '../app/models/Setting.php';
-        $settingModel = new Setting();
-        $setting = $settingModel->getOrCreate($user_id);
-        $new_mode = $setting['dark_mode'] ? 0 : 1;
-        $settingModel->updateDarkMode($user_id, $new_mode);
-        echo json_encode(['status' => 'success', 'dark_mode' => $new_mode]);
-        exit;
-    }
+    
     
 }
