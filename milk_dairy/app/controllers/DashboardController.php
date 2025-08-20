468f8cd6269cf5f946e01c1988a4ff11e5e56faf
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-20 07:14:16 +0530

    Auto commit on Wed, Aug 20, 2025  7:14:16 AM

--- milk_dairy/app/controllers/DashboardController.php
+++ milk_dairy/app/controllers/DashboardController.php
@@ -1,1 +1,12 @@
-
+<?php
+session_start();
+require_once '../app/models/Customer.php';
+require_once '../app/helpers/Auth.php';
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        Auth::check(); // If using auth
+        $this->view('dashboard/index');
+    }
+}
