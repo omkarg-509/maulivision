655c4f235a51c85cd8232dfcf72df3eedd7ca213
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-20 06:43:24 +0530

    Auto commit on Wed, Aug 20, 2025  6:43:24 AM

--- milk_dairy/app/models/User.php
+++ milk_dairy/app/models/User.php
@@ -47,4 +47,13 @@ class User extends Database
         $stmt->bind_param("si", $new_password, $id);
         return $stmt->execute();
     }
+
+    public function findByEmailOrNumber($email_or_number)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM vendor WHERE email = ? OR mobile_number = ? LIMIT 1");
+        $stmt->bind_param("ss", $email_or_number, $email_or_number);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
 }
