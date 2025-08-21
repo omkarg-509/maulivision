d1cc6793f1dae068d2716b92c67a26fc97b47e0a
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-21 15:32:35 +0530

    Auto commit on Thu, Aug 21, 2025  3:32:35 PM

--- milk_dairy/app/views/customer/view.php
+++ milk_dairy/app/views/customer/view.php
@@ -611,7 +611,7 @@ function showBillModal(billData) {
                     <button type="button" class="btn btn-primary" onclick="downloadPDF()">
                         <i class="fa fa-download"></i> Download PDF
                     </button>
-                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">Close</button>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close" class="btn btn-secondary">Close</button>
                 </div>
             </div>
         </div>
