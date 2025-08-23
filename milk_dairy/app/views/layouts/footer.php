bcf46fd067e55464725908c6c3803a2297018ded
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-23 11:18:48 +0530

    Auto commit on Sat, Aug 23, 2025 11:18:48 AM

--- milk_dairy/app/views/layouts/footer.php
+++ milk_dairy/app/views/layouts/footer.php
@@ -6,11 +6,6 @@
    <!-- General JS Scripts -->
   <script src="<?=BASE_URL?>assets/js/app.min.js"></script>
   <!-- JS Libraies -->
-  <!-- <script src="<?=BASE_URL?>assets/bundles/chartjs/chart.min.js"></script> -->
-  <!-- <script src="<?=BASE_URL?>assets/bundles/apexcharts/apexcharts.min.js"></script> -->
-  <!-- Page Specific JS File -->
-  <!-- <script src="<?=BASE_URL?>assets/js/page/index2.js"></script> -->
-  <!-- Template JS File -->
   <script src="<?=BASE_URL?>assets/js/scripts.js"></script>
   <!-- Custom JS File -->
   <script src="<?=BASE_URL?>assets/js/custom.js"></script>
