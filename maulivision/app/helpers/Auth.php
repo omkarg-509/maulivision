c6cbe9d50873fe6193e1e34940d9e32c548c6bd6
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-24 15:29:36 +0530

    Auto commit on Sun, Aug 24, 2025  3:29:35 PM

---
+++ maulivision/app/helpers/Auth.php
@@ -0,0 +1,38 @@
+<?php
+
+class Auth
+{
+    public static function check()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (!self::isLoggedIn()) {
+            header("Location: /public/auth/logout");
+            exit;
+        }
+    }
+
+    public static function isLoggedIn()
+    {
+        return isset($_SESSION['vendor']);
+    }
+
+    public static function user()
+    {
+        return $_SESSION['vendor'] ?? null;
+    }
+
+    public static function logout()
+    {
+        session_start();
+        session_destroy();
+
+        if (isset($_COOKIE['vendor'])) {
+            setcookie("vendor", "", time() - 3600, "/");
+        }
+
+        header("Location: /public/auth/login");
+        exit;
+    }
+}
