6f427e45104c2ceea17efc6a4c83a136500b49c5
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-24 15:45:57 +0530

    Auto commit on Sun, Aug 24, 2025  3:45:57 PM

--- maulivision/app/views/layouts/footer.php
+++ maulivision/app/views/layouts/footer.php
@@ -30,9 +30,3 @@
     </div>
   
   </footer>
-
-
-<script src="<?= BASE_URL ?>assets/js/plugins.js"></script>
-  <script src="<?= BASE_URL ?>assets/js/theme.js"></script>
-</body>
-</html>
---
+++ maulivision/app/views/layouts/tel.php
@@ -0,0 +1,7 @@
+
+
+
+<script src="<?= BASE_URL ?>assets/js/plugins.js"></script>
+  <script src="<?= BASE_URL ?>assets/js/theme.js"></script>
+</body>
+</html>
