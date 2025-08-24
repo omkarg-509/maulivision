a43f053637f1ce745cdaa98e3df8747733483079
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-25 00:33:53 +0530

    Auto commit on Mon, Aug 25, 2025 12:33:52 AM

--- maulivision/app/views/auth/login.php
+++ maulivision/app/views/auth/login.php
@@ -12,7 +12,7 @@
       <div class="row justify-content-center">
         <div class="col-12 col-md-10 col-lg-8 col-xl-6">
              <div class="card shadow-sm">
-<div class="row">
+<!-- <div class="row"> -->
            
 
               <div class="col-md-7">
