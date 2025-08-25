195b9aa61d42535042d73d6eebf8197f50a113dd
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-25 12:36:12 +0530

    Auto commit on Mon, Aug 25, 2025 12:36:12 PM

--- milk_dairy/public/index.php
+++ milk_dairy/public/index.php
@@ -5,7 +5,6 @@
 require_once '../config/config.php';  // Load configuration
 require_once '../core/App.php';   // Load application core
 require_once '../core/Controller.php';  // Load base controller
-require_once 'get_vendor_lang.php';
 
 $app = new App(); 
 
