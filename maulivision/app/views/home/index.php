96ce5b296f3272f3f3d072883df2553b6de64a41
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-25 15:34:27 +0530

    Auto commit on Mon, Aug 25, 2025  3:34:26 PM

--- maulivision/app/views/home/index.php
+++ maulivision/app/views/home/index.php
@@ -396,74 +396,7 @@
       <amp-auto-ads type="adsense"
         data-ad-client="ca-pub-6175226312586147">
 </amp-auto-ads>
-      <div class="overflow-hidden" id="Projects">
-        <div class="container pt-12 pt-lg-7 pb-14 pb-md-16">
-          <div class="row">
-            <div class="col-lg-10 col-xl-9 col-xxl-8 mx-auto text-center">
-              <h2 class="fs-16 text-uppercase text-primary mb-3">Latest Projects</h2>
-              <h3 class="display-3 mb-10">Check out some of our awesome projects with creative ideas and great design.</h3>
-            </div>
-            <!-- /column -->
-          </div>
-          <!-- /.row -->
-          <div class="container  nav-color mb-14" data-margin="30" data-dots="false" data-nav="false" data-items-md="2" data-items-xs="1">
-            <div class="overflow-visible">
-              <div class="wrapper">
-                
-                <div class="slide">
-                  <figure class="rounded mb-7"><a target="_blank" href="#"><img src="<?= BASE_URL ?>assets1/clients/aus.wabp"  alt="Aus Lighting Industry" /></a></figure>
-                  <div class="project-details d-flex justify-content-center flex-column">
-                    <div class="post-header text-center">
-                      <h2 class="post-title h3"><a target="_blank" class="link-dark" href="https://auslightingind.com/">Aus Lighting Industry</a></h2>
-                      <div class="post-category text-ash">light  manufacturing</div>
-                    </div>
-                    <!-- /.post-header text-center -->
-                  </div>
-                  <!-- /.project-details -->
-                </div>
-                <div class="slide">
-                  <figure class="rounded mb-7"><a target="_blank" href="#"><img src="<?= BASE_URL ?>assets1/clients/bessw.png"  alt="Client project" /></a></figure>
-                  <div class="project-details d-flex justify-content-center flex-column">
-                    <div class="post-header text-center">
-                      <h2 class="post-title h3"><a target="_blank" class="link-dark" href="#">Brand & E‑commerce</a></h2>
-                      <div class="post-category text-ash">design / storefront</div>
-                    </div>
-                  </div>
-                </div>
-                <div class="slide">
-                  <figure class="rounded mb-7"><a target="_blank" href="#"><img src="<?= BASE_URL ?>assets1/clients/pas.png"  alt="Client project" /></a></figure>
-                  <div class="project-details d-flex justify-content-center flex-column">
-                    <div class="post-header text-center">
-                      <h2 class="post-title h3"><a target="_blank" class="link-dark" href="#">Local Business Site</a></h2>
-                      <div class="post-category text-ash">website / seo</div>
-                    </div>
-                  </div>
-                </div>
-                <div class="slide">
-                  <figure class="rounded mb-7"><a target="_blank" href="#"><img src="<?= BASE_URL ?>assets1/clients/sbcn.png"  alt="Client project" /></a></figure>
-                  <div class="project-details d-flex justify-content-center flex-column">
-                    <div class="post-header text-center">
-                      <h2 class="post-title h3"><a target="_blank" class="link-dark" href="#">Corporate Landing</a></h2>
-                      <div class="post-category text-ash">ui / performance</div>
-                    </div>
-                  </div>
-                </div>
-             
-           
-              </div>
-              <!--/.wrapper -->
-            </div>
-            <!-- /.-->
-          </div>
-          <!-- /.container -->
-        </div>
-        <!-- /.container -->
-        <div class="divider text-soft-primary mx-n2">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100">
-            <path fill="currentColor" d="M1260,1.65c-60-5.07-119.82,2.47-179.83,10.13s-120,11.48-180,9.57-120-7.66-180-6.42c-60,1.63-120,11.21-180,16a1129.52,1129.52,0,0,1-180,0c-60-4.78-120-14.36-180-19.14S60,7,30,7H0v93H1440V30.89C1380.07,23.2,1319.93,6.15,1260,1.65Z" />
-          </svg>
-        </div>
-      </div>
+    
       
 
       <div class="overflow-hidden" id="Projects">
