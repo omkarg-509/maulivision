ac8a1dce9950dad780123adffcba4351c6a6f125
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-26 09:49:35 +0530

    Auto commit on Tue, Aug 26, 2025  9:49:35 AM

--- maulivision/app/views/auth/login.php
+++ maulivision/app/views/auth/login.php
@@ -18,7 +18,7 @@
             <div class="row g-0">
               <div class="col-md-5 d-none d-md-flex align-items-center justify-content-center login-left p-4">
                 <div class="text-center">
-                  <img src="assets1/favicon-1.png" alt="MauliVision" style="height:64px" class="mb-3">
+                  <img src="<?= BASE_URL ?>/public/assets1/favicon-1.png" alt="MauliVision" style="height:64px" class="mb-3">
                   <h5 class="mb-0">Welcome Back</h5>
                   <small class="d-block small-note">MAULI VISION</small>
                 </div>
