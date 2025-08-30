4eb95ea298a6e200f32e842de19587e2de10e1fa
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-30 11:50:04 +0530

    Auto commit on Sat, Aug 30, 2025 11:50:04 AM

--- milk_dairy/app/controllers/CustomerController.php
+++ milk_dairy/app/controllers/CustomerController.php
@@ -175,6 +175,15 @@ if (file_exists($fontPath)) {
             $pdf->Cell(95, 8, 'to: ' . date('d/m/Y', strtotime($endDate)), 1, 1);
 
             $pdf->Ln(5);
+require_once __DIR__ . '/vendor/autoload.php';
+
+$mpdf = new \Mpdf\Mpdf([
+    'default_font' => 'NotoSerifDevanagari' // देवनागरी font वापर
+]);
+
+$html = '<h1 style="text-align:center;">श्री कृष्णा डेअरी & स्वीट</h1>';
+$mpdf->WriteHTML($html);
+$mpdf->Output("dairy.pdf", "I"); // "I" म्हणजे browser मध्ये open होईलcd ..
 
             // Generate date range
             $dateRange = [];
