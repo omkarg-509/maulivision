b49dc37d4df411e4dd5e292429a54290b8d378bf
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-30 11:17:33 +0530

    Auto commit on Sat, Aug 30, 2025 11:17:33 AM

--- milk_dairy/app/controllers/CustomerController.php
+++ milk_dairy/app/controllers/CustomerController.php
@@ -100,7 +100,7 @@ class CustomerController extends Controller
 
             // Create PDF with proper error handling
             $pdf = new TCPDF();
-            $pdf->SetFont('notosansdevanagari', 'B', 20);
+          
             $pdf->SetCreator($businessName . ' System');
             $pdf->SetAuthor($businessName);
             $pdf->SetTitle('दूध बिल - ' . $customer['name']);
