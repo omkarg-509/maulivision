bdfcafa5c2a38af467c88c500a923c7de0dd4327
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-30 16:43:23 +0530

    Auto commit on Sat, Aug 30, 2025  4:43:22 PM

--- maulivision/app/controllers/DashboardController.php
+++ maulivision/app/controllers/DashboardController.php
@@ -3,6 +3,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
+  require_once '../app/views/layouts/sidebar.php';
         $this->view('dashboard/index');
     }
 }
