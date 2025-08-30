65ccf6204926890f9d74675c26b2dedf0f957c85
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-30 13:09:02 +0530

    Auto commit on Sat, Aug 30, 2025  1:09:02 PM

--- milk_dairy/app/views/customer/view.php
+++ milk_dairy/app/views/customer/view.php
@@ -273,12 +273,12 @@ $(document).ready(function() {
     
     filteredEntries = [...allEntries];
     
-    // // Set current month as default (1st day to last day of current month)
-    // const now = new Date();
-    // // Set first day of current month
-    // const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
-    // // Set last day of current month
-    // const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+    // Set current month as default (1st day to last day of current month)
+    const now = new Date();
+    // Set first day of current month
+    const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+    // Set last day of current month
+    const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
 
     
     // Set the date inputs to current month range
