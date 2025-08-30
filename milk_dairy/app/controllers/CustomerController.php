28fe6624c77bfe125e4739136c8585000f68d658
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-30 11:27:30 +0530

    Auto commit on Sat, Aug 30, 2025 11:27:29 AM

--- milk_dairy/app/controllers/CustomerController.php
+++ milk_dairy/app/controllers/CustomerController.php
@@ -101,23 +101,12 @@ class CustomerController extends Controller
             // Create PDF with proper error handling
             $pdf = new TCPDF();
 
-            // // Register and use NotoSansDevanagari font for Marathi
-            // $fontPath = __DIR__ . '/../lib/tcpdf/fonts/notosansdevanagari.php';
-            // if (file_exists($fontPath)) {
-            //     $pdf->AddFont('notosansdevanagari', '', 'notosansdevanagari.php');
-            // }
-            // $pdf->SetFont('notosansdevanagari', 'B', 20);
-$fontPath = __DIR__ . '/../lib/tcpdf/fonts/NotoSansDevanagari-Regular.ttf';
-if (file_exists($fontPath)) {
-    $fontname = TCPDF_FONTS::addTTFfont($fontPath, 'TrueTypeUnicode', '', 32);
-}
-// Marathi Customer Name
-$pdf->SetFont('notosansdevanagari', '', 11);
-$pdf->Cell(95, 8, 'Customer: ' . $customer['name'], 1, 0);
-
-// English Bill No
-$pdf->SetFont('helvetica', '', 11);
-$pdf->Cell(95, 8, 'Bill No: ' . $customer['bill_id'], 1, 0);
+            // Register and use NotoSansDevanagari font for Marathi
+            $fontPath = __DIR__ . '/../lib/tcpdf/fonts/notosansdevanagari.php';
+            if (file_exists($fontPath)) {
+                $pdf->AddFont('notosansdevanagari', '', 'notosansdevanagari.php');
+            }
+            $pdf->SetFont('notosansdevanagari', 'B', 20);
 
             $pdf->SetCreator($businessName . ' System');
             $pdf->SetAuthor($businessName);
@@ -192,7 +181,17 @@ $pdf->Cell(95, 8, 'Bill No: ' . $customer['bill_id'], 1, 0);
                 $dateRange[$dateStr] = isset($dailyMilk[$dateStr]) ? $dailyMilk[$dateStr] : ['cow' => 0, 'buffalo' => 0];
                 $currentDate->modify('+1 day');
             }
+$fontPath = __DIR__ . '/../lib/tcpdf/fonts/NotoSansDevanagari-Regular.ttf';
+if (file_exists($fontPath)) {
+    $fontname = TCPDF_FONTS::addTTFfont($fontPath, 'TrueTypeUnicode', '', 32);
+}
+// Marathi Customer Name
+$pdf->SetFont('notosansdevanagari', '', 11);
+$pdf->Cell(95, 8, 'Customer: ' . $customer['name'], 1, 0);
 
+// English Bill No
+$pdf->SetFont('helvetica', '', 11);
+$pdf->Cell(95, 8, 'Bill No: ' . $customer['bill_id'], 1, 0);
             // Daily milk table - English only
             $html = '
             <style>
