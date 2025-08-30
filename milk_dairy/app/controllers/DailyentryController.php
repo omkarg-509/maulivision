c29faf168774b02e2a78dbe7cd5c0096e16bf2fa
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-30 12:51:19 +0530

    Auto commit on Sat, Aug 30, 2025 12:51:18 PM

--- milk_dairy/app/controllers/DailyentryController.php
+++ milk_dairy/app/controllers/DailyentryController.php
@@ -25,11 +25,11 @@ class DailyentryController extends Controller
                 'cid' => $_POST['cid'] ?? '',
                 'milktype' => $_POST['milktype'] ?? '',
                 'milkliter' => $_POST['milkliter'] ?? '',
-                'entrydate' => $_POST['selected_date'] ?? date('Y-m-d'),
+                'entry_date' => $_POST['selected_date'] ?? date('Y-m-d'),
             ];
 
             // Debug: Check if all required fields are present
-            if (empty($data['vid']) || empty($data['cid']) || empty($data['milktype']) || empty($data['milkliter']) || empty($data['entrydate'])) {
+            if (empty($data['vid']) || empty($data['cid']) || empty($data['milktype']) || empty($data['milkliter']) || empty($data['entry_date'])) {
                 header('Content-Type: application/json');
                 echo json_encode(['success' => false, 'message' => 'All fields are required.', 'data' => $data]);
                 exit;
