346576b65ba3eb049f131233747b72348a98a534
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-08-30 12:49:30 +0530

    Auto commit on Sat, Aug 30, 2025 12:49:30 PM

--- milk_dairy/app/models/DailyEntry.php
+++ milk_dairy/app/models/DailyEntry.php
@@ -35,7 +35,7 @@ class DailyEntry extends Database
             $created_at = date('Y-m-d H:i:s');
         }
 
-        $stmt = $this->db->prepare("INSERT INTO daily_entries (vid,cid,milktype,milkliter,selected_date,created_at) VALUES (?,?,?,?,?,?)");
+        $stmt = $this->db->prepare("INSERT INTO daily_entries (vid,cid,milktype,milkliter,selected_date,created_at) VALUES (?,?,?,?,?,?,?)");
         $stmt->bind_param("iisdss", $data['vid'], $data['cid'], $data['milktype'], $data['milkliter'],$data['entrydate'], $created_at);
         $success = $stmt->execute();
         $stmt->close();
