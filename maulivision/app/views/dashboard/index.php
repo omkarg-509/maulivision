813c82778840c1216bbc084013bf8f6482aab2ae
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-02 14:00:55 +0530

    Auto commit on Tue, Sep  2, 2025  2:00:55 PM

--- maulivision/app/views/dashboard/index.php
+++ maulivision/app/views/dashboard/index.php
@@ -17,7 +17,7 @@
                   <div class="padding-20">
                     <div class="text-end">
                       <h5 class="font-light mb-0 text-dark">
-                        <i class="ti-arrow-up text-success"></i><?= $data['vendorCount'] ?>
+                        <i class="ti-arrow-up text-success"></i><?= $vendorCount ?>
                     </div>
                   </div>
                 </div> </a>
