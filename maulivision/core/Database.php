21227f4d7007404330ba940b01b64a80609a821b
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-02 14:03:43 +0530

    Auto commit on Tue, Sep  2, 2025  2:03:43 PM

--- maulivision/core/Database.php
+++ maulivision/core/Database.php
@@ -3,7 +3,7 @@
 class Database
 {
     protected $db;
-    protected $db2;
+    // protected $db2;
 
     public function __construct()
     {
@@ -12,9 +12,9 @@ class Database
             die("Database connection failed: " . $this->db->connect_error);
         }
 
-        $this->db2 = new mysqli("localhost", "u367009900_milk_dairy", "AC]WO/mL9", "u367009900_milk_dairy");
-    if ($this->db2->connect_error) {
-        die("Database milk_dairy connection failed: " . $this->db2->connect_error);
-    }
+    //     $this->db2 = new mysqli("localhost", "u367009900_milk_dairy", "AC]WO/mL9", "u367009900_milk_dairy");
+    // if ($this->db2->connect_error) {
+    //     die("Database milk_dairy connection failed: " . $this->db2->connect_error);
+    // }
     }
 }
