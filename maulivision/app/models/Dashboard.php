d5d6833a37a463537f711cf02c18722df2ce5553
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-02 15:16:02 +0530

    Auto commit on Tue, Sep  2, 2025  3:16:02 PM

--- maulivision/app/models/Dashboard.php
+++ maulivision/app/models/Dashboard.php
@@ -4,5 +4,12 @@ require_once '../core/Database.php';
 
 class Dashboard extends Database
 {
-
+    public function countVendor()
+    {
+        $sql = "SELECT COUNT(*) as total FROM vendor";
+        $stmt = $this->db2->prepare($sql);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row['total'];
+    }
 }
