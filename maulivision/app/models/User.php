a7ac12b749964daf8b28b6812a6e56ed3e830735
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-03 00:42:33 +0530

    Auto commit on Wed, Sep  3, 2025 12:42:33 AM

--- maulivision/app/models/User.php
+++ maulivision/app/models/User.php
@@ -4,55 +4,7 @@ require_once '../core/Database.php';
 
 class User extends Database
 {
-    public function findByEmail($email)
-    {
-        $stmt = $this->db->prepare("SELECT * FROM superadmin WHERE email = ? LIMIT 1");
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
-    }
-
-    public function create($data)
-    {
-        // Align columns with DB schema order
-        $stmt = $this->db->prepare("INSERT INTO superadmin (name, business_name, business_number, business_address, email, mobile_number, password) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param(
-            "sssssss",
-            $data['name'],
-            $data['business_name'],
-            $data['business_number'],
-            $data['business_address'],
-            $data['email'],
-            $data['mobile_number'],
-            $data['password']
-        );
-        if ($stmt->execute()) {
-            $id = $this->db->insert_id;
-            $stmt->close();
-            return $id;
-        }
-        $stmt->close();
-        return false;
-    }
-
-    public function findByEmailAndNumber($email, $number)
-    {
-        $stmt = $this->db->prepare("SELECT * FROM superadmin WHERE email = ? AND mobile = ? LIMIT 1");
-        $stmt->bind_param("ss", $email, $number);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
-    }
-
-    public function updatePassword($id, $new_password)
-    {
-        $stmt = $this->db->prepare("UPDATE superadmin SET password = ? WHERE id = ?");
-        $stmt->bind_param("si", $new_password, $id);
-        return $stmt->execute();
-    }
-
+   
     public function findByEmailOrNumber($email_or_number)
     {
         $stmt = $this->db->prepare("SELECT * FROM superadmin WHERE email = ? OR mobile = ? LIMIT 1");
@@ -62,12 +14,4 @@ class User extends Database
         return $result->fetch_assoc();
     }
 
-    public function findById($id)
-    {
-        $stmt = $this->db->prepare("SELECT * FROM superadmin WHERE id = ? LIMIT 1");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
-    }
 }
