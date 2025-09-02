957432b3e95526a79d549f2d260a8ced441a1e3d
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-03 01:02:56 +0530

    Auto commit on Wed, Sep  3, 2025  1:02:56 AM

--- maulivision/app/controllers/DashboardController.php
+++ maulivision/app/controllers/DashboardController.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__ . '/../helpers/Auth.php';
+require_once __DIR__ . '/../models/Dashboard.php';
+
 class DashboardController extends Controller
 {
     public function index()
