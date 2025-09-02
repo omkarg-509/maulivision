0039bf08b23b6f59de4144e9a4ce0f1b2a7ffc20
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-03 01:00:20 +0530

    Auto commit on Wed, Sep  3, 2025  1:00:20 AM

--- maulivision/app/models/Dashboard.php
+++ maulivision/app/models/Dashboard.php
@@ -7,8 +7,10 @@ class Dashboard extends Database
     public function getSuperAdminCount()
     {
         $result = $this->db->query("SELECT COUNT(*) AS count FROM superadmin");
-        $row = $result->fetch_assoc();
-        return $row['count'];
+        if ($result && $row = $result->fetch_assoc()) {
+            return $row['count'];
+        }
+        return 0;
     }
 
  
