c81b86f69e9a31f1325c16c103cc841422f1e2b0
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-02 14:15:14 +0530

    Auto commit on Tue, Sep  2, 2025  2:15:14 PM

--- maulivision/app/controllers/DashboardController.php
+++ maulivision/app/controllers/DashboardController.php
@@ -4,10 +4,10 @@ class DashboardController extends Controller
     public function index()
     {
         // Auth::check(); // ✅ session check
-        // $dashboardModel = $this->model('Dashboard');
-        // $vendorCount = $dashboardModel->countVendorModels();
-        // $this->view('dashboard/index', ['vendorCount' => $vendorCount]);
-        $this->view('dashboard/index');
+        $dashboardModel = $this->model('Dashboard');
+        $vendorCount = $dashboardModel->countVendorModels();
+        $this->view('dashboard/index', ['vendorCount' => $vendorCount]);
+        // $this->view('dashboard/index');
 
     }
 
