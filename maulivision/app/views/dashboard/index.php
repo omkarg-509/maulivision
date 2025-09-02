b0f34b24c0895d3c3a3778531983e271caf53d94
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-03 01:23:20 +0530

    Auto commit on Wed, Sep  3, 2025  1:23:20 AM

--- maulivision/app/views/dashboard/index.php
+++ maulivision/app/views/dashboard/index.php
@@ -19,7 +19,7 @@
                       <div class="text-muted small">Vendors</div>
                       <h5 class="font-light mb-0 text-dark">
                         <i class="ti-arrow-up text-success"></i>
-                        <?= isset($data['superadminCount']) ? (int)$data['superadminCount'] : 0 ?>  
+                        <?= isset($data['superadminData']) ? (int)$data['superadminData'] : 0 ?>  
                       </h5>
                     </div>
                   </div>
