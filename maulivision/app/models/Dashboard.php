b5b857b11f8e4fcb9ff33a728457b5d3e51c0dd9
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-02 14:51:41 +0530

    Auto commit on Tue, Sep  2, 2025  2:51:41 PM

--- maulivision/app/models/Dashboard.php
+++ maulivision/app/models/Dashboard.php
@@ -4,4 +4,10 @@ require_once '../core/Database.php';
 
 class Dashboard extends Database
 {
+    public function getAllVendors()
+    {
+        $stmt = $this->db->prepare("SELECT * FROM superadmin");
+        $stmt->execute();
+        return $stmt->fetch_all(MYSQLI_ASSOC);
+    }
 }
