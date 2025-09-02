3beb5d297dc1ccd63eaaf8c7ce4e08e04af89e31
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-02 15:35:34 +0530

    Auto commit on Tue, Sep  2, 2025  3:35:34 PM

--- maulivision/app/views/dashboard/index.php
+++ maulivision/app/views/dashboard/index.php
@@ -11,26 +11,33 @@
 
         
              <div class="col-lg-3 col-md-6 col-sm-6 col-6">
-              <div class="card card-statistic-1"><a href="dashboard/vendors">
+              <div class="card card-statistic-1"><a href="<?= BASE_URL ?>dashboard/vendors">
                 <i class="fas fa-users card-icon col-green"></i>
                 <div class="card-wrap">
                   <div class="padding-20">
                     <div class="text-end">
+                      <div class="text-muted small">Vendors</div>
                       <h5 class="font-light mb-0 text-dark">
-                        <i class="ti-arrow-up text-success"></i><?= $vendorCount ?>
+                        <i class="ti-arrow-up text-success"></i>
+                        <?= isset($vendorCount) ? (int)$vendorCount : 0; ?>
+                      </h5>
                     </div>
                   </div>
                 </div> </a>
               </div>
             </div>
                <div class="col-lg-3 col-md-6 col-sm-6 col-6">
-              <div class="card card-statistic-1"><a href="../loan/loan.php">
+              <div class="card card-statistic-1"><a href="#">
                 <i class="fas fa-rupee-sign card-icon col-green"></i>
                 <div class="card-wrap">
                   <div class="padding-20">
                     <div class="text-end">
+                      <div class="text-muted small">Revenue</div>
                       <h5 class="font-light mb-0 text-dark">
-                        <i class="ti-arrow-up text-success"></i><?//= $data['dailyentry'] ?>
+                        <i class="ti-arrow-up text-success"></i>
+                        <!-- TODO: Wire a real metric here -->
+                        0
+                      </h5>
                     </div>
                   </div>
                 </div> </a>
