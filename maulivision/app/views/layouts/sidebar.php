8fb0a85c83fdadfa4f068761d0dba050f9d8621c
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-09 15:41:23 +0530

    Auto commit on Tue, Sep  9, 2025  3:41:23 PM

--- maulivision/app/views/layouts/sidebar.php
+++ maulivision/app/views/layouts/sidebar.php
@@ -56,6 +56,14 @@
                 
               </ul>
             </li>
+            <li class="dropdown">
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-users"></i><span>Own system</span></a>
+              <ul class="dropdown-menu">
+                <li class=""><a class="nav-link" href="<?=BASE_URL?>customer/create">Create Customers</a></li>
+                <li><a class="nav-link" href="<?=BASE_URL?>customer/index">Customers Details</a></li>
+                
+              </ul>
+            </li>
            
           <li class="">
               <a href="<?=BASE_URL?>auth/logout" class="nav-link"><i class="fas fa-sign-out-alt"></i><span>Logout</span></a>
