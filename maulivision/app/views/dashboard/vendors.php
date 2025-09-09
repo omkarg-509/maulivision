39e5ae96ff71db5b3cdc2ab6e4dee92a8e8ed1c0
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-09 15:34:17 +0530

    Auto commit on Tue, Sep  9, 2025  3:34:17 PM

--- maulivision/app/views/dashboard/vendors.php
+++ maulivision/app/views/dashboard/vendors.php
@@ -1,55 +1,23 @@
-<?php require_once '../app/views/layouts/sidebar.php';?>
+<?php require_once '../app/views/layouts/sidebar.php'; ?>
 
 
 <div class="main-content">
   <div class="loader"></div>
-  <div id="app"> 
+  <div id="app">
 
-        <section class="section mt-5">
-
-         <div class="row">
-
-        
-             <div class="col-lg-3 col-md-6 col-sm-6 col-6">
-              <div class="card card-statistic-1"><a href="dashboard/vendors">
-                <i class="fas fa-users card-icon col-green"></i>
-                <div class="card-wrap">
-                  <div class="padding-20">
-                    <div class="text-end">
-                      <h5 class="font-light mb-0 text-dark">
-                        <i class="ti-arrow-up text-success"></i><?//= $data['customerCount'] ?>
-                    </div>
-                  </div>
-                </div> </a>
-              </div>
-            </div>
-               <div class="col-lg-3 col-md-6 col-sm-6 col-6">
-              <div class="card card-statistic-1"><a href="../loan/loan.php">
-                <i class="fas fa-rupee-sign card-icon col-green"></i>
-                <div class="card-wrap">
-                  <div class="padding-20">
-                    <div class="text-end">
-                      <h5 class="font-light mb-0 text-dark">
-                        <i class="ti-arrow-up text-success"></i><?//= $data['dailyentry'] ?>
-                    </div>
-                  </div>
-                </div> </a>
-              </div>
-            </div>
-        
-
-        
-            
-          </div>
-             </section>
+    <section class="section mt-5">
+      <div class="row">
+      </div>
+    </section>
 
 
 
 
- 
-        </div>
 
-   
-   
-    
+
   </div>
+
+
+
+
+</div>
