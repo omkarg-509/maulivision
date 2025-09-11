b4910bd067eef981cb235ee896fdf12c1df19805
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-11 11:24:08 +0530

    Auto commit on Thu, Sep 11, 2025 11:24:08 AM

--- mcms/app/views/layouts/header.php
+++ mcms/app/views/layouts/header.php
@@ -12,6 +12,16 @@
   <!-- Custom style CSS -->
   <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/custom.css">
   <link rel='shortcut icon' type='image/x-icon' href='<?= BASE_URL ?>assets/img/logo-1.png' />
+  <style>
+    link[rel="shortcut icon"] + style,
+    link[rel="icon"] + style {
+      /* fallback for favicon, not applicable */
+    }
+    /* Round the logo in the page header if used as an <img> */
+    .logo-img, .favicon-img {
+      border-radius: 50%;
+    }
+  </style>
 </head>
 
 
