5462cb9fd0f9f31c77f9f1201cc6c027fecadd9f
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-13 10:56:29 +0530

    Auto commit on Sat, Sep 13, 2025 10:56:29 AM

--- mcms/app/models/Customers.php
+++ mcms/app/models/Customers.php
@@ -7,20 +7,20 @@ class Customers extends Database
      
     public function getAll()
     {
-        $result = $this->db->query("SELECT * FROM customers");
+        $result = $this->db->query("SELECT * FROM mcms_customers ORDER BY id DESC");
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     public function insert($data)
     {
-        $stmt = $this->db->prepare("INSERT INTO customers (vid,name,mobile,in_time,amount,staff,payment_method) VALUES (?,?,?,?,?,?,?)");
+        $stmt = $this->db->prepare("INSERT INTO mcms_customers ORDER BY id DESC (vid,name,mobile,in_time,amount,staff,payment_method) VALUES (?,?,?,?,?,?,?)");
         $stmt->bind_param("isssdss", $data['vid'], $data['name'], $data['mobile'], $data['in_time'], $data['amount'],$data['staff'] ,$data['payment_method']);
         $stmt->execute();
     }
 
     public function delete($id)
     {
-        $stmt = $this->db->prepare("DELETE FROM customers WHERE id = ?");
+        $stmt = $this->db->prepare("DELETE FROM mcms_customers ORDER BY id DESC WHERE id = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
     }
