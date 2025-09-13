0a665128634f938267e4fe2e1d66207446365f00
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-13 12:57:16 +0530

    Auto commit on Sat, Sep 13, 2025 12:57:15 PM

--- mcms/app/controllers/CustomersController.php
+++ mcms/app/controllers/CustomersController.php
@@ -17,7 +17,7 @@ class CustomersController extends Controller
         Auth::check();
     $customersModel = $this->model('Customers');
     $vid = $_SESSION['vendor']['id'] ?? null;
-    $customers = $vid ? $customersModel->getByVendorUniqueMobile((int)$vid) : [];
+    $customers =  $customersModel->getAll();
     $this->view('customers/history', ['customers' => $customers]);
     }
     public function customers(){
