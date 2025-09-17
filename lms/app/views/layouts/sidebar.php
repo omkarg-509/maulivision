02a29c3836c4a5f08c8597b875793469aed4e2eb
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-09-17 12:30:43 +0530

    Auto commit on Wed, Sep 17, 2025 12:30:43 PM

--- lms/app/views/layouts/sidebar.php
+++ lms/app/views/layouts/sidebar.php
@@ -1,14 +1,13 @@
  <div class="navbar-bg"></div>
       <nav class="navbar navbar-expand-lg main-navbar">
         <div class="form-inline me-auto">
-          <ul class="navbar-nav navbar-left">
-        
-          <a href="<?=BASE_URL?>dashboard" 
-              class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-              <img alt="image" src="<?=BASE_URL?>/assets/img/logo-1.png" class="">
-
+        <ul class="navbar-nav navbar-left">
+          <li>
+            <a href="<?=BASE_URL?>dashboard" 
+                class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="<?=BASE_URL?>/assets/img/logo-1.png" class="">
             </a>
-          
+          </li>
         </ul>
           
         </div>
@@ -23,12 +22,12 @@
           <div class="sidebar-brand">
             <a href="index.html">
             <?php
+            <?php
               $vendor = $_SESSION['vendor'] ?? [];
-              $logoName = $vendor['bussines_name']
-                ?? ($vendor['bussiness_name'] ?? ($vendor['bussiness_name'] ?? ($vendor['bussiness_name'] ?? 'Massage Center')));
+              $logoName = $vendor['business_name']
+                ?? ($vendor['bussines_name'] ?? 'Massage Center');
             ?>
             <span class="logo-name"><?= htmlspecialchars($logoName) ?></span>
-            </a>
           </div>
           <ul class="sidebar-menu">
             <li class="menu-header">Main</li>
