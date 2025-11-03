b7e529af5fe47d4be4428ba16418a81ae5bcb7f7
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-11-03 14:30:14 +0530

    Auto commit on Mon, Nov  3, 2025  2:30:13 PM

--- mcms/app/controllers/AuthController.php
+++ mcms/app/controllers/AuthController.php
@@ -41,7 +41,7 @@ public function login()
 
         // Password check (plain fallback). Replace with password_verify when hashes deployed.
         $valid = $vendor && (
-            (isset($vendor['password']) && $vendor['password'] === $password)
+            (isset($vendor['password']) && $vendor['password'] === password_hash($password, PASSWORD_DEFAULT))
             || (isset($vendor['password']) && password_get_info($vendor['password'])['algo'] !== 0 && password_verify($password, $vendor['password']))
         );
 
