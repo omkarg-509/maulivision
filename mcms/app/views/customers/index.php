4d617d18c8853f6e6654442bfa70e41a7682a639
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-11-07 10:46:27 +0530

    Auto commit on Fri, Nov  7, 2025 10:46:26 AM

--- mcms/app/views/customers/index.php
+++ mcms/app/views/customers/index.php
@@ -55,7 +55,7 @@
                     </div>
                     <div class="row">
                       <div class="col-sm-9 offset-sm-3 text-center">
-                        <button type="submit" class="btn btn-primary btn-sm px-4" id="saveBtn">Submit</button>
+                        <button type="submit" class="btn btn-primary btn-sm px-4" >Submit</button>
                       </div>
                     </div>
                   </div>
