af2fab7f38129cc2b80c184b941acd80b749c63a
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-11-07 10:38:15 +0530

    Auto commit on Fri, Nov  7, 2025 10:38:15 AM

--- mcms/app/views/customers/index.php
+++ mcms/app/views/customers/index.php
@@ -14,7 +14,7 @@
                   <div class="card-header">
                     <h4>Add Customer</h4>
                   </div>
-                <form id="customerForm" method="POST" action="customers/store">
+                <form id="customerForm" method="POST" action="customer/store">
                   <div class="card-body">
                     <!-- vid is set server-side from session; no client-provided vid -->
                     <div class="row mb-2 align-items-center">
