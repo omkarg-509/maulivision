5edddf8cbd5d872e27046f109c35d64737c59644
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-11-07 11:08:05 +0530

    Auto commit on Fri, Nov  7, 2025 11:08:05 AM

--- mcms/app/views/dashboard/index.php
+++ mcms/app/views/dashboard/index.php
@@ -68,7 +68,7 @@
                 </div>
                  <div class="col-6 col-md-3 col-lg-3">
                           <div class="card  btn btn-default">
-                              <a class="card-body  text-center mt-4 mb-2 " href="<?= BASE_URL?>contacts/number"><i class="fas fa-phone-right" style="font-size:30px ;"></i> </a><a>Customers Number</a>
+                              <a class="card-body  text-center mt-4 mb-2 " href="<?= BASE_URL?>contacts/number"><i class="fas fa-phone" style="font-size:30px ;"></i> </a><a>Customers Number</a>
                           </div>
                 </div>
       </section>
