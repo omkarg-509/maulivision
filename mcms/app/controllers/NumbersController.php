9959a175b3afec69f6875ff221573545758b1eee
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-11-07 11:11:00 +0530

    Auto commit on Fri, Nov  7, 2025 11:11:00 AM

--- mcms/app/controllers/NumbersController.php
+++ mcms/app/controllers/NumbersController.php
@@ -5,6 +5,6 @@ class HomeController extends Controller
     public function index()
     {
          Auth::check(); // 🔒 Protect the home page
-        $this->view('constacts/number');
+        $this->view('constacts/numbers');
     }
 }
