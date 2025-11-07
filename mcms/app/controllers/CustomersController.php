38ab2a3f4f5e1158b950d2cb49d2288557ee4d23
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-11-07 10:54:24 +0530

    Auto commit on Fri, Nov  7, 2025 10:54:24 AM

--- mcms/app/controllers/CustomersController.php
+++ mcms/app/controllers/CustomersController.php
@@ -41,9 +41,9 @@ class CustomersController extends Controller
         echo $staff = trim($_POST['staff'] ?? '');
         echo $payment_method = trim($_POST['payment_method'] ?? '');
 
-        $vendorModel = $this->model('customers');
-        $id=$vendorModel->insert($vid,$name,$mobile,$in_time,$amount,$staff,$payment_method);
-       
+        $customersModel = $this->model('customers');
+        $id=$customersModel->insert($vid,$name,$mobile,$in_time,$amount,$staff,$payment_method);
+
             header('Location: ' . BASE_URL . 'customers/index');
             return;
       
