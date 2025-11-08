19846454e1a51d10fc6c24394e067cf498a06d22
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-11-08 12:03:51 +0530

    Auto commit on Sat, Nov  8, 2025 12:03:51 PM

--- mcms/app/helpers/Auth.php
+++ mcms/app/helpers/Auth.php
@@ -5,7 +5,7 @@ class Auth
     public static function check()
     {
         session_start();
-         session_destroy();
+        //  session_destroy();
         if (!self::isLoggedIn()) {
             header("Location: /public/auth/login");
             exit;
