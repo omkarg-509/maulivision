8609fe7cd8284268fa17866c4253da48a7793797
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-12-15 11:14:31 +0530

    Auto commit on Mon, Dec 15, 2025 11:14:31 AM

--- mcms/app/views/layouts/sidebar.php
+++ mcms/app/views/layouts/sidebar.php
@@ -45,6 +45,14 @@
                 <li><a class="nav-link" href="<?=BASE_URL?>customers/customers">All Customers</a></li>
               </ul>
             </li>
+              <li class="dropdown">
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-users"></i><span>Expenses</span></a>
+              <ul class="dropdown-menu">
+                <li class=""><a class="nav-link" href="<?=BASE_URL?>expenses/index">New Expenses</a></lianzu
+                <li><a class="nav-link" href="<?=BASE_URL?>expenses/history">History</a></li>
+                <li><a class="nav-link" href="<?=BASE_URL?>expenses/expenses">All Expenses</a></li>
+              </ul>
+            </li>
 
             <!-- <li class="dropdown">
               <a href="#" class="nav-link has-dropdown"><i class="fas fa-user-tie"></i><span>Staff</span></a>
