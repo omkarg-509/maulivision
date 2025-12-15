fe64a4f12181ba9feacf32dedfab774609af4a14
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-12-15 12:04:20 +0530

    Auto commit on Mon, Dec 15, 2025 12:04:20 PM

--- mcms/app/views/expenses/index.php
+++ mcms/app/views/expenses/index.php
@@ -1,5 +1,6 @@
 <?php require_once '../app/views/layouts/sidebar.php'; ?>
 <div class="main-content">
+      <div id="app">
   <section class="section">
     <div class="section-header d-flex align-items-center flex-wrap gap-2">
       <h1 class="mb-0">Finance Manager</h1>
