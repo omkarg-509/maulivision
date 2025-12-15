57213db33d9d83156ef3d228a88c49492e72458d
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-12-15 11:16:16 +0530

    Auto commit on Mon, Dec 15, 2025 11:16:15 AM

--- mcms/app/views/expenses/index.php
+++ mcms/app/views/expenses/index.php
@@ -12,7 +12,7 @@
              
                 <div class="">
                   <div class="col-12 text-center mb-4">
-                    <h4>Add Customer</h4>
+                    <h4>Expenses</h4>
                   </div>
                 <form id="customerForm" method="POST" action="<?= BASE_URL ?>customers/store">
                   <div class="">
