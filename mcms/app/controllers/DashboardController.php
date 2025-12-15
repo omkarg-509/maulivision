302320c1fde2576add2eaee2fca46bddbd735b1e
Omkar Vivek Gaikwad <omkarg509@gmail.com>
2025-12-15 11:10:42 +0530

    Auto commit on Mon, Dec 15, 2025 11:10:42 AM

--- mcms/app/controllers/DashboardController.php
+++ mcms/app/controllers/DashboardController.php
@@ -1,12 +1,11 @@
 <?php
-require_once '../app/models/Customer.php';
 require_once '../app/helpers/Auth.php';
 class DashboardController extends Controller
 {
     public function index()
     {
         Auth::check(); // If using auth
-        $customerModel = new Customer();
+        $customerModel = $this->model('Dashboard');
         $dailyEarning = $customerModel->dailyEarning();
         $monthlyEarning = $customerModel->monthlyEarning();
         $yearlyEarning = $customerModel->yearlyEarning();
